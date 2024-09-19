15ace97ef867a6173a2dce54933295e8980bff19
Alefe Queiroz <alefequeiroz17@gmail.com>
2024-09-19 15:42:09 -0300

    C# Prep 2—Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    {   
+        Console.WriteLine("Type your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90 )
+        {   
+            Console.WriteLine("Your final grade is: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your final grade is: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your final grade is: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your final grade is: D");
+        }
+        else{
+            Console.WriteLine("Your final grade is: F");
+        }
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations!!!");
+        }
+        else{
+            Console.WriteLine("you did not meet the College grade requeriments.");
+        }
+    }  
 }
