70d91db03c13caacbd3bc4ca25a64009b95c2733
Alefe Queiroz <alefequeiroz17@gmail.com>
2024-09-20 15:12:29 -0300

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
-class Program
-{
+
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    {   
+        Console.WriteLine(" What is the magic number?");
+        int magicnumber = int.Parse(Console.ReadLine());
+        Console.WriteLine(" What is your guess?");
+        int guess = int.Parse(Console.ReadLine());
+
+        while(magicnumber != guess)
+        {   
+            Console.WriteLine(" What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            if(guess < magicnumber){
+                Console.WriteLine("Higher");
+            }
+            else if(guess > magicnumber){
+                Console.WriteLine("Lower");
+            }
+        }
+        Console.WriteLine($"yes! > {magicnumber} < is the magic number!");
+        }
 }
