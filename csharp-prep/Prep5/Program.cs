6311899af0de2b382e947b37f8de8f22c2c66ce3
Alefe <alefequeiroz17@gmail.com>
2024-09-21 14:12:06 -0300

    5. Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int squaredNumber = SquareNumber();
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber()
+    {
+        int number = PromptUserNumber();
+        return number * number;
+    }
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your favorite number is {squaredNumber}.");
     }
 }
