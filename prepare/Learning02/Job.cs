c59c3e469c44f002a44c6fa53d1bc0b0acac4c55
Alefe <alefequeiroz17@gmail.com>
2024-09-24 21:02:53 -0300

    Resume file created

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 public class Job
 {
     public string _company = "";
@@ -10,26 +9,5 @@ public class Job
     public void DisplayJobDetails()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-    }
-}
-public class Resume
-{
-    public string _name = "";
-    public List<Job> _jobs = new List<Job>();
-
-    public Resume(string name)
-    {
-        _name = name;
-    }
-
-    public void Display()
-    {
-        Console.WriteLine($"Name: {_name}");
-        Console.WriteLine("Jobs:");
-
-        foreach (var job in _jobs)
-        {
-            job.DisplayJobDetails();
-        }
     }
 }
